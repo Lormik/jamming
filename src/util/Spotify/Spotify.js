718b53473dc9cb1a699b6ab3af275f02153f6c51
Mike Lord <73435010+Lormik@users.noreply.github.com>
2020-11-20 16:53:22 +0000

    update search to spotify apo

--- src/util/Spotify/Spotify.js
+++ src/util/Spotify/Spotify.js
@@ -4,6 +4,7 @@ const redirectUri = "http://localhost:3000/";
 let accessToken;
 
 const Spotify = {
+
     getAccessToken() {
         if (accessToken) {
             return accessToken;
@@ -23,9 +24,27 @@ const Spotify = {
 
             window.location = accessURL;
         }
-    }
+    },
 
-    
+    search() {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, { headers: { Authorization: `Bearer ${accessToken}` } }).then(
+            response => {
+                return response.json();
+        }).then(
+            jsonResponse => {
+                if (!jsonResponse.tracks) {
+                    return [];
+                }
+                return jsonResponse.tracks.items.map(track => ({
+                    id: track.id,
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    uri: track.uri
+                }));
+        });
+    },
     
 };
 
