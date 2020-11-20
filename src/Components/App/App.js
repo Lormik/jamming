60d966047a21767277bb390bf82fbd35aa326462
Mike Lord <73435010+Lormik@users.noreply.github.com>
2020-11-20 12:55:33 +0000

    update playlist test data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     this.state = {
       searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 }, { name: 'name2', artist: 'artist2', album: 'album2', id: 2 }, {  name: 'name3', artist: 'artist3', album: 'album3', id: 3 }],
       playlistName: 'playlistname1',
-      playlistTracks: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 }, { name: 'name2', artist: 'artist2', album: 'album2', id: 2 }, {  name: 'name3', artist: 'artist3', album: 'album3', id: 3 }]
+      playlistTracks: [{ name: 'nameA', artist: 'artistA', album: 'albumA', id: 1 }, { name: 'nameB', artist: 'artistB', album: 'albumB', id: 2 }, {  name: 'nameC', artist: 'artistC', album: 'albumC', id: 3 }]
     };
   }
   render() {
